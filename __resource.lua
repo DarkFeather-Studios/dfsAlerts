8f5d445c7039f7d0e8076e8faf1e9ef8edda1b61
Daemon Brooks <54357300+xotikorukx@users.noreply.github.com>
2020-08-17 14:46:01 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,3 +2,10 @@ client_scripts {
     'client.lua'
 }
 server_script 'server.lua'
+
+dependencies {
+    'dfs',
+    'mythic_notify',
+    'dfs_PoliceJob', --unreleased. Only used for IsCop().
+    'dfs_dbperms'
+}
